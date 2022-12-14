8ce19cb9ea38af00a445ca8c83bdb6e88fb6c514
Lisandro1012 <LisandroRodriguez1012@gmail.com>
2022-12-14 10:47:03 -0400

    mysql cambios

--- dist/data-source/mysql.js
+++ dist/data-source/mysql.js
@@ -11,16 +11,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 //import { createPool, Pool } from "mysql2/promise";
 const promise_1 = require("mysql2/promise");
-const config_1 = require("../config");
 function connect() {
     return __awaiter(this, void 0, void 0, function* () {
         const connection = yield promise_1.createPool({
-            user: config_1.DB_USER,
-            password: config_1.DB_PASSWORD,
-            host: config_1.DB_HOST,
-            port: config_1.DB_PORT,
-            database: config_1.DB_NAME,
+            host: "localhost",
+            user: "root",
+            password: "josePass1012$",
+            database: "venezuladb",
         });
+        connection.query("SELECT * FROM estados");
         return connection;
     });
 }
--- src/data-source/mysql.ts
+++ src/data-source/mysql.ts
@@ -4,12 +4,30 @@ import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from "../config";
 
 export async function connect(): Promise<Pool> {
   const connection = await createPool({
-    user: DB_USER,
-    password: DB_PASSWORD,
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME,
+    host: "localhost",
+    user: "root",
+    password: "josePass1012$",
+    database: "venezuladb",
+    // host: "localhost:3306",
+    // user: "root",
+    // password: "Jose1012$",
+    // database: "venezuladb",
+
+    // host: "containers-us-west-104.railway.app",
+    // user: "root",
+    // password: "OTJVS2vtQnTikEFi8uym",
+    // database: "railway",
+    // port: 3306,
+
+    // user: DB_USER,
+    // password: DB_PASSWORD,
+    // host: DB_HOST,
+    // port: DB_PORT,
+    // database: DB_NAME,
   });
+
+  connection.query("SELECT * FROM estados");
+
   return connection;
 }
 
